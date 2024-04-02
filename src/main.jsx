d43a4300c4d2e2e2a9f3b1f297020a7a395c57f2
lasernoy <cloudy.nny07@gmail.com>
2024-04-02 14:37:47 +0800

    css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Clock from './components/Clock.jsx'
+import Clock25Plus5 from './components/Clock25Plus5.jsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Clock />
+    <Clock25Plus5 />
   </React.StrictMode>,
 )
